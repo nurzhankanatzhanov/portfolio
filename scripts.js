14a7154263107792bb7efb4c886c0fb7043de802
Nurzhan Kanatzhanov <nurzhan.kanatzhanov@wustl.edu>
2020-01-07 18:28:36 -0600

    finished button styling

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function downloadCV(){
+   let link = 'https://nurzhankanatzhanov.github.io/portfolio/resume.pdf';
+    window.open(link, '_blank');
+}
+
+function emailMe(){
+    let link = 'mailto:nurzhan.kanatzhanov@gmail.com'
+    location.href = link;
+}
+
+function connect(){
+    let link = 'https://www.linkedin.com/in/nurzhan-kanatzhanov/';
+    window.open(link, '_blank');
+}
