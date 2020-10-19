603811e35c9dfe37d1de941c7add638c48965e23
Nurzhan Kanatzhanov <nurzhan@Nurzhans-MacBook-Pro.local>
2020-10-19 17:26:12 -0500

    updated page

--- scripts.js
+++ scripts.js
@@ -1,43 +1,48 @@
 // button scripts
 function downloadCV() {
-  let link = "https://nurzhankanatzhanov.github.io/portfolio/Resume.pdf";
-  window.open(link, "_blank");
+  let link = 'https://nurzhankanatzhanov.github.io/portfolio/Resume.pdf';
+  window.open(link, '_blank');
 }
 
 function emailMe() {
-  let link = "mailto:nurzhan.kanatzhanov@gmail.com";
+  let link = 'mailto:nurzhan.kanatzhanov@gmail.com';
   location.href = link;
 }
 
 function connect() {
-  let link = "https://www.linkedin.com/in/nurzhan-kanatzhanov/";
-  window.open(link, "_blank");
+  let link = 'https://www.linkedin.com/in/nurzhan-kanatzhanov/';
+  window.open(link, '_blank');
 }
 
 function viewSample() {
-  let link = "https://nurzhankanatzhanov.github.io/portfolio/Final_Project.pdf";
-  window.open(link, "_blank");
+  let link = 'https://nurzhankanatzhanov.github.io/portfolio/Final_Project.pdf';
+  window.open(link, '_blank');
+}
+
+function viewUdemy() {
+  let link = 'https://udemy-burger-61040.web.app/';
+  window.open(link, '_blank');
 }
 
 function viewDB2() {
-  let link = "https://databasedashboard.co/";
-  window.open(link, "_blank");
+  let link = 'https://databasedashboard.co/';
+  window.open(link, '_blank');
 }
 
 function viewHTML(c) {
-  var link = "";
+  var link = '';
   if (c === 1) {
     link =
-      "https://nurzhankanatzhanov.github.io/portfolio/lyrics_comparison.html";
+      'https://nurzhankanatzhanov.github.io/portfolio/lyrics_comparison.html';
   } else if (c === 2) {
     link =
-      "https://nurzhankanatzhanov.github.io/portfolio/lyrics_comparison.pdf";
+      'https://nurzhankanatzhanov.github.io/portfolio/lyrics_comparison.pdf';
   } else if (c === 3) {
     link =
-      "https://nurzhankanatzhanov.github.io/portfolio/lyrics_comparison.ipynb";
+      'https://nurzhankanatzhanov.github.io/portfolio/lyrics_comparison.ipynb';
   } else {
-    alert("Error");
+    alert('Error');
   }
 
-  window.open(link, "_blank");
+  window.open(link, '_blank');
 }
